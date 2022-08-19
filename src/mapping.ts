3374e67c43a4aa58fab6558d188a4ef9d3928538
JaredTokuz <jtokuz@gmail.com>
2022-08-18 21:44:03 -0500

    nice

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,36 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import {
+  LogMint,
+  LogWithdraw,
+} from "../generated/SimpleBondingCurve/SimpleBondingCurve";
+import { TokenTrade } from "../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleMintEvent(event: LogMint): void {
+  // log.info("mint dude = {} hexstring = {} string = {} from = {} hash = {}", [
+  //   event.transaction.nonce.toHex(),
+  //   event.transaction.nonce.toHexString(),
+  //   event.transaction.nonce.toString(),
+  //   event.transaction.from.toHex(),
+  //   event.transaction.hash.toHex(),
+  // ]);
+  const tokenTrade = new TokenTrade(event.transaction.hash);
+  tokenTrade.nonce = event.transaction.nonce;
+  tokenTrade.er20Amount = event.params.amountMinted;
+  tokenTrade.ethAmount = event.params.totalCost;
+  tokenTrade.timestamp = event.block.timestamp;
+  tokenTrade.tradeType = "Mint";
+  tokenTrade.address = event.transaction.from.toHexString();
+  tokenTrade.save();
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleWithdrawEvent(event: LogWithdraw): void {
+  // log.info("withdraw dude", []);
+  const tokenTrade = new TokenTrade(event.transaction.hash);
+  tokenTrade.nonce = event.transaction.nonce;
+  tokenTrade.er20Amount = event.params.amountWithdrawn;
+  tokenTrade.ethAmount = event.params.reward;
+  tokenTrade.timestamp = event.block.timestamp;
+  tokenTrade.tradeType = "Burn";
+  tokenTrade.address = event.transaction.from.toHexString();
+  tokenTrade.save();
 }
